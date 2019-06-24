5e2cfec783d36e20c16dc9393e134acc1a70b1d0
Pascal Young <pascal@novaoflove.com>
2019-06-24 14:21:44 -0700

    Require reply command to be used in DMs as well

--- cogs/commands/reply.py
+++ cogs/commands/reply.py
@@ -11,35 +11,40 @@ class Reply(commands.Cog):
 
     @commands.command()
     async def reply(self, ctx):
-        try:
-            conn = sqlite3.connect('anon.db')
-            c = conn.cursor()
-            stuff = ctx.message.content.split(' ')
-            thread_id, message = stuff[1], ' '.join(stuff[2:])
-            user = ctx.author
-            receiver = user.id
-            anon_id = c.execute(
-                f"SELECT anon_sender FROM anon_messages WHERE thread_id={thread_id}").fetchone()[0]
-            anon = discord.utils.get(self.bot.users, id=anon_id)
+        if type(ctx.channel) != discord.DMChannel:
+            await ctx.send(f"Send your message to the bots DMs!")
+            await ctx.message.delete()
+        else:
+            try:
+                conn = sqlite3.connect('anon.db')
+                c = conn.cursor()
+                stuff = ctx.message.content.split(' ')
+                thread_id, message = stuff[1], ' '.join(stuff[2:])
+                user = ctx.author
+                receiver = user.id
+                anon_id = c.execute(
+                    f"SELECT anon_sender FROM anon_messages WHERE thread_id={thread_id}").fetchone()[0]
+                anon = discord.utils.get(self.bot.users, id=anon_id)
 
-            # insert data
-            # storing replies isn't necessary, but its there if needed
-            data = ("REPLY", thread_id, anon_id, receiver, message)
-            c.execute('INSERT INTO anon_messages VALUES (null,?,?,?,?,?)', data)
-            conn.commit()
+                # insert data
+                # storing replies isn't necessary, but its there if needed
+                data = ("REPLY", thread_id, anon_id, receiver, message)
+                c.execute(
+                    'INSERT INTO anon_messages VALUES (null,?,?,?,?,?)', data)
+                conn.commit()
 
-            embed = discord.Embed(
-                title="Your message was replied to!", color=0x267d28,
-                description=f'Use `!reply {thread_id} <msg>` to respond')
-            embed.add_field(
-                name='Thread ID:', value=thread_id, inline=True)
-            embed.add_field(
-                name='Message:', value=message, inline=True)
-            await ctx.send(embed=embed)
-            await user.send('Sent! :mailbox_with_mail:')
-        except AttributeError:
-            await ctx.send(f'Unknown message thread!')
-        conn.close()
+                embed = discord.Embed(
+                    title="Your message was replied to!", color=0x267d28,
+                    description=f'Use `!reply {thread_id} <msg>` to respond')
+                embed.add_field(
+                    name='Thread ID:', value=thread_id, inline=True)
+                embed.add_field(
+                    name='Message:', value=message, inline=True)
+                await ctx.send(embed=embed)
+                await user.send('Sent! :mailbox_with_mail:')
+            except AttributeError:
+                await ctx.send(f'Unknown message thread!')
+            conn.close()
 
 
 def setup(bot):

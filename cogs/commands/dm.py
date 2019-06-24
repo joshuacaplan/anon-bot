4977744376bfb82c48da5e412716a82d5e1e345d
TheStrplum213 <str.plumber2005straja@gmail.com>
2019-06-24 22:59:28 +0200

    Added "Send it to the DMs!"

--- cogs/commands/dm.py
+++ cogs/commands/dm.py
@@ -11,38 +11,42 @@ class DM(commands.Cog):
 
     @commands.command()
     async def dm(self, ctx):
-        try:
-            conn = sqlite3.connect('anon.db')
-            c = conn.cursor()
-            stuff = ctx.message.content.split(' ')
-            user, message = stuff[1], ' '.join(stuff[2:])
-            user = discord.utils.get(self.bot.users, name=user)
-            anon = ctx.author
-            receiver = user.id
-            anon_sender = ctx.author.id
+        if type(ctx.channel) != discord.DMChannel:
+            await ctx.send(f"Send your message to the bots DMs!")
+            await ctx.message.delete()
+        else:
+            try:
+                conn = sqlite3.connect('anon.db')
+                c = conn.cursor()
+                stuff = ctx.message.content.split(' ')
+                user, message = stuff[1], ' '.join(stuff[2:])
+                user = discord.utils.get(self.bot.users, name=user)
+                anon = ctx.author
+                receiver = user.id
+                anon_sender = ctx.author.id
 
-            # generates thread_id and checks if it exists
-            while True:
-                try:
-                    thread_id = ''.join((str(randint(0, 9))
-                                         for _ in range(10)))
-                    c.execute(
-                        f"SELECT receiver FROM anon_messages WHERE thread_id={thread_id}")
-                    # if no rows exist, break out of regen loop
-                    thread_data = c.fetchone()[0]
-                except TypeError:
-                    break
+                # generates thread_id and checks if it exists
+                while True:
+                    try:
+                        thread_id = ''.join((str(randint(0, 9))
+                                             for _ in range(10)))
+                        c.execute(
+                            f"SELECT receiver FROM anon_messages WHERE thread_id={thread_id}")
+                        # if no rows exist, break out of regen loop
+                        thread_data = c.fetchone()[0]
+                    except TypeError:
+                        break
 
-            # insert data
-            data = ("NEW_THREAD", thread_id, anon_sender, receiver, message)
-            c.execute('INSERT INTO anon_messages VALUES (null,?,?,?,?,?)', data)
-            conn.commit()
+                # insert data
+                data = ("NEW_THREAD", thread_id, anon_sender, receiver, message)
+                c.execute('INSERT INTO anon_messages VALUES (null,?,?,?,?,?)', data)
+                conn.commit()
 
-            await user.send(f'You got an anonymous message! Thread: {thread_id}\nMessage:\n{message}\nUse `!reply {thread_id} <msg>` to respond')
-            await anon.send('Sent! :mailbox_with_mail:')
-        except AttributeError:
-            await ctx.send(f'A user with that name could not be found.')
-        conn.close()
+                await user.send(f'You got an anonymous message! Thread: {thread_id}\nMessage:\n{message}\nUse `!reply {thread_id} <msg>` to respond')
+                await anon.send('Sent! :mailbox_with_mail:')
+            except AttributeError:
+                await ctx.send(f'A user with that name could not be found.')
+            conn.close()
 
 
 def setup(bot):

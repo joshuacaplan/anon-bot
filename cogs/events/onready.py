52980f1eb6f64f917b9ff07aae3b781f9ed33305
Pascal Young <pascal@novaoflove.com>
2019-06-24 12:11:37 -0700

    db: setup database on ready

--- cogs/events/onready.py
+++ cogs/events/onready.py
@@ -1,14 +1,25 @@
 import discord
+import sqlite3
 from discord.ext import commands
+
 
 class OnReady(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Logged In')
+        print(
+            f'Logged in as {self.bot.user.name}#{self.bot.user.discriminator} (ID: {self.bot.user.id})')
         await self.bot.change_presence(activity=discord.Game('!dm to use'))
+
+        # setup database connection
+        conn = sqlite3.connect('anon.db')
+        c = conn.cursor()
+        c.execute('''CREATE TABLE IF NOT EXISTS anon_messages
+                (event_id INTEGER PRIMARY KEY AUTOINCREMENT, sender INTEGER, receiver INTEGER, message TEXT)''')
+        conn.commit()
+
 
 def setup(bot):
     bot.add_cog(OnReady(bot))

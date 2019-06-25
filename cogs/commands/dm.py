5962c60a15f6ded251b6d192635815ec9e4c8ad9
TheStrplum213 <30937619+TheStrplum213@users.noreply.github.com>
2019-06-25 20:02:15 +0200

    Update dm.py

--- cogs/commands/dm.py
+++ cogs/commands/dm.py
@@ -11,7 +11,7 @@ class DM(commands.Cog):
 
     @commands.command()
     async def dm(self, ctx):
-        if type(ctx.channel) is not discord.DMChannel:
+        if type(ctx.channel) != discord.DMChannel:
             await ctx.send(f"Send your message to the bots DMs!")
             await ctx.message.delete()
         else:

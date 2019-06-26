4a5918e7a62435b66b1b003969d657b5b25cd673
Elijah Lopez <elijahllopezz@gmail.com>
2019-06-26 12:54:08 -0400

    Update reply.py
    Formatting

--- cogs/commands/reply.py
+++ cogs/commands/reply.py
@@ -21,29 +21,23 @@ class Reply(commands.Cog):
                 args = ctx.message.content.split(' ')
                 thread_id, message = args[1], ' '.join(args[2:])
                 user = ctx.author
-                receiver_id = c.execute(
-                    f'SELECT receiver FROM threads WHERE thread_id={thread_id}').fetchone()[0]
+                receiver_id = c.execute(f'SELECT receiver FROM threads WHERE thread_id={thread_id}').fetchone()[0]
                 embed_title = f'{user} replied to your message!' if receiver_id == user.id else 'You got another message'
                 if receiver_id == user.id:
-                    receiver_id = c.execute(
-                        f'SELECT anon_sender FROM threads WHERE thread_id={thread_id}').fetchone()[0]
+                    receiver_id = c.execute(f'SELECT anon_sender FROM threads WHERE thread_id={thread_id}').fetchone()[0]
                 receiver = discord.utils.get(self.bot.users, id=receiver_id)
-                message_id = c.execute(
-                    f'SELECT max(message_id) FROM messages WHERE thread_id={thread_id}').fetchone()[0]
+                message_id = c.execute(f'SELECT max(message_id) FROM messages WHERE thread_id={thread_id}').fetchone()[0]
 
                 # insert data
                 # storing replies isn't necessary, but its there if needed
                 message_data = (thread_id, message_id + 1, user.id, message)
-                c.execute(
-                    'INSERT INTO messages VALUES (?,?,?,?)', message_data)
+                c.execute('INSERT INTO messages VALUES (?,?,?,?)', message_data)
                 conn.commit()
                 embed = discord.Embed(
                     title=embed_title, color=0x267d28,
                     description=f'Use `!reply {thread_id} <msg>` to respond')
-                embed.add_field(
-                    name='Thread ID:', value=thread_id, inline=True)
-                embed.add_field(
-                    name='Message:', value=message, inline=True)
+                embed.add_field(name='Thread ID:', value=thread_id, inline=True)
+                embed.add_field(name='Message:', value=message, inline=True)
                 await receiver.send(embed=embed)
                 await ctx.send('Reply sent! :mailbox_with_mail:')
             except (AttributeError, TypeError):

202876ec0868e3f25ee4dbf5b227bc08c709c0db
Elijah Lopez <elijahllopezz@gmail.com>
2019-06-27 00:16:13 -0400

    Update purgeDatabase.py

--- cogs/commands/purgeDatabase.py
+++ cogs/commands/purgeDatabase.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from subprocess import Popen
 from Globals import bot_owners
 import os
+import sqlite3
 
 
 my_user_id = int(os.environ['my_user_id']) or 0
@@ -16,12 +17,25 @@ class PurgeDatabase(commands.Cog):
         if ctx.author.id in bot_owners:
             try:
                 os.remove('anon.db')
-                await ctx.send('Database removed! Restarting now..')
+                conn = sqlite3.connect('anon.db')
+                c = conn.cursor()
+                c.execute('''CREATE TABLE IF NOT EXISTS threads
+                        (event_id INTEGER PRIMARY KEY AUTOINCREMENT, thread_id INTEGER, anon_sender INTEGER, receiver INTEGER)''')
+                c.execute('''CREATE TABLE IF NOT EXISTS messages
+                        (thread_id INTEGER, message_id INTEGER, sender INTEGER, message TEXT)''')
+                c.execute('''CREATE TABLE IF NOT EXISTS reports
+                        (report_id INTEGER PRIMARY KEY AUTOINCREMENT, thread_id INTEGER, anon_sender INTEGER, reporter INTEGER, details TEXT)''')
+                c.execute('''CREATE TABLE IF NOT EXISTS guildOptions
+                        (event_id INTEGER PRIMARY KEY AUTOINCREMENT, guild_id INTEGER, report_channel_id INTEGER)''')
+                c.execute('''CREATE TABLE IF NOT EXISTS userOptions
+                        (event_id INTEGER PRIMARY KEY AUTOINCREMENT, user_id INTEGER, allow_anon_messages BOOLEAN)''')
+                c.execute('''CREATE TABLE IF NOT EXISTS guildFilters
+                        (words TEXT, server_id INTEGER)''')
+                conn.commit()
+                conn.close()
+                await ctx.send('Database purged!')
             except PermissionError:
                 await ctx.send('Unable to purge database. Is another process using it?')
-            await self.bot.change_presence(activity=discord.Game('Restarting...'))
-            Popen('python bot.py')
-            await self.bot.logout()
         else:
             await ctx.send(f'You do not have permission to use this command!')
 
